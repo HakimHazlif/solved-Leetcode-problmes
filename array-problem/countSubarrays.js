df408aaca73d29defc4211c73e147678dbde788a
Hakim-56 <hakimhazlif@gmail.com>
2025-04-27 13:29:23 +0100

    Solve 3392. Count Subarrays of Length Three With a Condition problem

---
+++ array-problem/countSubarrays.js
@@ -0,0 +1,9 @@
+const countSubarrays = function (nums) {
+  let counter = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if ((nums[i] + nums[i + 2]) * 2 === nums[i + 1]) counter++;
+  }
+
+  return counter;
+};
