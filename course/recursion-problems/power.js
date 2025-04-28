c4e8e6a1ee91677014e81cae9d1e36cf73c3400c
Hakim-56 <hakimhazlif@gmail.com>
2025-04-28 11:58:51 +0100

    refactor power function

--- course/recursion-problems/power.js
+++ course/recursion-problems/power.js
@@ -2,9 +2,8 @@
 
 function power(base, exponent) {
   if (exponent === 0) return 1;
-  if (exponent === 1) return base;
 
-  const powered = power(base, exponent - 1);
-
-  return powered * base;
+  return base * power(base, exponent - 1);
 }
+
+console.log(power(2, 4));
