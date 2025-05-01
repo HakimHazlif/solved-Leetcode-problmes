4e29af0fedc24940b7576a1e49259dca145035ea
Hakim-56 <hakimhazlif@gmail.com>
2025-05-01 13:37:45 +0100

    Recognize the bubble sort algorithm

---
+++ course/sorting-algths/bubbleSort/bubbleSort.js
@@ -0,0 +1,32 @@
+// function bubbleSort(arr) {
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         // SWAP
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+// }
+
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  let noSwap;
+
+  for (let i = arr.length; i > 0; i--) {
+    noSwap = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        noSwap = false;
+      }
+    }
+
+    if (noSwap) break;
+  }
+}
